b86993857334d83c6286de014626ac1fac43088b
donghonggang <hunter_dong58@qq.com>
2021-01-09 19:33:08 +0800

    test

--- My.GitHub.Web/Controllers/HomeController.cs
+++ My.GitHub.Web/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace My.GitHub.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page322231.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page322222222.";
 
             return View();
         }
